a3440e52d0354d0e33ab55e4e35a5835d3d1c16b
jcamara <camarajoao1@gmail.com>
2023-03-13 21:14:29 -0700

    create get request structure. get request is working.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const videoRoutes = require("./routes/videos")
+const videoRoutes = require("./routes/videos");
+const cors = require("cors");
 
 const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+app.use(videoRoutes);
+
+
 
 app.listen(8080, function () {
     console.log('the server is running on port 8080');
--- routes/videos.js
+++ routes/videos.js
@@ -1,3 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const fs = require("fs");
+
+
+router.get("/", function (request, response) {
+    const videosFile = fs.readFileSync("./data/videos.json");
+    const videosArray = JSON.parse(videosFile);
+    response.json(videosArray)
+})
+
+router.get("/videos/:videoId", function (request, response) {
+    console.log(request.params);
+    const videosFile = fs.readFileSync("./data/videos.json");
+    const videosArray = JSON.parse(videosFile);
+    const videoWithId = videosArray.find((video) => {
+        return video.id === request.params.videoId;
+    })
+    if (!videoWithId) {
+        response.status(404);
+        response.json([]);
+        return;
+    }
+    response.json(videoWithId);
+})
+
+module.exports = router;
+
