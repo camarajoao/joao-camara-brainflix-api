97a5df551940bfe2815fc294eb7d05d6fd9330e9
jcamara <camarajoao1@gmail.com>
2023-03-16 18:24:50 -0700

    add router post

--- routes/videos.js
+++ routes/videos.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const fs = require("fs");
+const uniqid = require('uniqid');
+
+// const data = require("./data/videos.json");
 
 
 router.get("/", function (request, response) {
@@ -10,7 +13,6 @@ router.get("/", function (request, response) {
 })
 
 router.get("/videos/:videoId", function (request, response) {
-    console.log(request.params);
     const videosFile = fs.readFileSync("./data/videos.json");
     const videosArray = JSON.parse(videosFile);
     const videoWithId = videosArray.find((video) => {
@@ -22,6 +24,33 @@ router.get("/videos/:videoId", function (request, response) {
         return;
     }
     response.json(videoWithId);
+})
+
+router.post("/upload", (request, response) => {
+
+    console.log(request.body);
+    const newVideo = request.body;
+    newVideo.id = uniqid();
+    newVideo.title = request.body.title;
+    newVideo.channel = "User Channel";
+    newVideo.image = "http://localhost:8080/Upload-video-preview.jpg";
+    newVideo.description = request.body.description;
+    newVideo.views = "1";
+    newVideo.likes = "1";
+    newVideo.duration = "3:00";
+    newVideo.video = "";
+    newVideo.timestamp = Date.now();
+    newVideo.comments = [];
+
+
+    const videosFile = fs.readFileSync("./data/videos.json");
+    const videosArray = JSON.parse(videosFile);
+
+    videosArray.push(newVideo);
+
+    fs.writeFileSync("./data/videos.json", JSON.stringify(videosArray));
+    console.log(videosArray);
+    response.json(videosArray)
 })
 
 module.exports = router;
